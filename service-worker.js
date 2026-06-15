9cc077dac43f1a49bc8df0ec83ba87be5d8a3d2a
Fupudav <rlopez.perso@gmail.com>
2026-06-15 23:56:38 +0200

    Simplify BladeBase navigation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2026.06.15.06";
+const APP_VERSION = "2026.06.15.07";
 const APP_CACHE = `bladebase-app-${APP_VERSION}`;
 const STATIC_CACHE = `bladebase-static-${APP_VERSION}`;
 
