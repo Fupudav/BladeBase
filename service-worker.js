95915a982936f3fb63bc6a796b28270e5cd7ef76
Fupudav <rlopez.perso@gmail.com>
2026-06-19 09:33:06 +0200

    Modernize collection dashboard UX

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2026.06.19.03";
+const APP_VERSION = "2026.06.19.04";
 const APP_CACHE = `bladebase-app-${APP_VERSION}`;
 const STATIC_CACHE = `bladebase-static-${APP_VERSION}`;
 
