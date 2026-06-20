390911089d56acd501572c59de42fb49afb30243
Fupudav <rlopez.perso@gmail.com>
2026-06-20 20:23:11 +0200

    Show balance completion type

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2026.06.19.06";
+const APP_VERSION = "2026.06.20.01";
 const APP_CACHE = `bladebase-app-${APP_VERSION}`;
 const STATIC_CACHE = `bladebase-static-${APP_VERSION}`;
 
